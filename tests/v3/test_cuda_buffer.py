bdb7934522c460821233b58f5e0ae0e5be8689e9
Mads R. B. Kristensen <madsbk@gmail.com>
2024-06-14 18:45:12 +0200

    more test

--- tests/v3/test_cuda_buffer.py
+++ tests/v3/test_cuda_buffer.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import numpy as np
 import pytest
 
 from zarr.array import AsyncArray
@@ -34,10 +35,25 @@ async def test_async_array():
     )
     expect[5:] = cupy.arange(5)
 
+    # Write into device memory
     await a.setitem(
         selection=(slice(5, 10),),
         value=cupy.arange(5),
         prototype=cuda_prototype,
     )
+    # Read into device memory
     got = await a.getitem(selection=(slice(0, 10),), prototype=cuda_prototype)
+    assert isinstance(got, cupy.ndarray)
     assert cupy.array_equal(expect, got)
+
+    # Write into host memory
+    await a.setitem(
+        selection=(slice(0, 5),),
+        value=np.arange(5),
+    )
+    expect[0:5] = cupy.arange(5)
+
+    # Read into host memory
+    got = await a.getitem(selection=(slice(0, 10),))
+    assert isinstance(got, np.ndarray)
+    assert np.array_equal(cupy.asnumpy(expect), got)
